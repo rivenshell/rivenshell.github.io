7feab6b4991aeea1429a4be77cceff658dbe9720
Rivenshell <rivenshell111@gmail.com>
2022-12-01 16:10:10 -0500

    Title Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       
       </div>
       <h1>Rivenshell's Portfolio</h1>
       <div className="card">
